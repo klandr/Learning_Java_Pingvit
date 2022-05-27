9acc2afcda811035f1db70336e357dacaf416f1f
klandr <pozand47@gmail.com>
2022-05-27 14:39:43 +0200

    initial commit

--- src/hw2/Task3.java
+++ src/hw2/Task3.java
@@ -2,6 +2,15 @@ package hw2;
 
 public class Task3 {
     public static void main(String[] args) {
+        //int i = 1;
+        int a = 0 ;
+        for (int i = 1;i<=256;i=i+i) {
+            //System.out.print(i + " lkjg ");
 
+            a=a+i;
+           // System.out.println(a);
+        }
+        System.out.println("sum=" + a);
     }
+
 }
