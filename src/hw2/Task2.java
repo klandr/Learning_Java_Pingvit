4fb58916e61cb7e1f8abe1375295136e2f2c6000
klandr <pozand47@gmail.com>
2022-05-27 13:42:29 +0200

    initial commit

--- src/hw2/Task2.java
+++ src/hw2/Task2.java
@@ -1,4 +1,16 @@
 package hw2;
 
 public class Task2 {
+    public static void main(String[] args) {
+        int a = 1;
+                for (int i=3;i<=24;i=i+3){
+                    a=a+a;
+                    System.out.print("за " + i + " години буде ");
+                    System.out.println(a + " амеб");
+
+            }
+
+
+
+    }
 }
--- src/hw2/Task4.java
+++ src/hw2/Task4.java
@@ -1,4 +1,20 @@
 package hw2;
 
 public class Task4 {
+    public static void main(String[] args) {
+        int a = 1;
+        int s = 2;
+        int d = 3;
+        int f = 0;
+        while (a < d) {
+            ;
+            a = a + 1;
+            // System.out.println(s);
+            //s=s+s;
+            f = f + s;
+            System.out.println(f);
+
+        }
+
+    }
 }
