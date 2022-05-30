c957dc755129767a9d78c676c5dfdf74b0bb9e20
klandr <pozand47@gmail.com>
2022-05-30 16:39:26 +0200

    task 6 up to 100 from 2

--- src/hw2/Task6.java
+++ src/hw2/Task6.java
@@ -3,7 +3,7 @@ package hw2;
 public class Task6 {
     public static void main(String[] args) {
         float z;
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 2; i <= 100; i++) {
             z = i % 2;
             if (z == 0) {
                 System.out.println(i);
