c4238556288843388ec8aa73181a488386360113
klandr <pozand47@gmail.com>
2022-05-30 17:02:25 +0200

    task 7 start

--- src/hw2/Task7.java
+++ src/hw2/Task7.java
@@ -1,4 +1,21 @@
 package hw2;
 
 public class Task7 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        float z = 0 ;
+        for (int i = 1;i<=9;i++){
+        z = i % 2;
+
+        //b = a+i;
+        if (z == 1) { //yakshcho ne parne to
+            a = i;
+            System.out.println(a);
+            //System.out.println(b);
+            //System.out.println(a+b);
+            }
+                }
+        //System.out.println(a+b); summa chysel
+    }
 }
