e304f6c5e1af345efb2c50f7ed1c248df672db8c
klandr <pozand47@gmail.com>
2022-05-30 17:42:22 +0200

    task 9 start

--- src/hw2/Task9.java
+++ src/hw2/Task9.java
@@ -1,4 +1,12 @@
 package hw2;
 
 public class Task9 {
+    public static void main(String[] args) {
+        char a = '*';
+        //System.out.println(a);
+        for (int i =0;i<=4;i++)
+
+            System.out.println(a);
+
+    }
 }
