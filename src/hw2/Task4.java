f484711599acf6a2609cfe97b984f9f4031949a1
klandr <pozand47@gmail.com>
2022-05-30 16:33:11 +0200

    task 6

--- src/hw2/Task4.java
+++ src/hw2/Task4.java
@@ -2,16 +2,16 @@ package hw2;
 
 public class Task4 {
     public static void main(String[] args) {
-        //int i = 1;
+
         int a = 10; // chyslo A
         int b = 8; // chyslo B
         int d = 0; // dodatkowy parametr summa
 
-        for (int i = 1;  i<=b; i++) {
-            d=d+a;
+        for (int i = 1; i <= b; i++) {
+            d = d + a;
             //System.out.print(a + " * " + i + " = ");
             //System.out.println(d);
         }
-        System.out.println(a + " * " + b + " = "+ d);
+        System.out.println(a + " * " + b + " = " + d);
     }
 }
--- src/hw2/Task6.java
+++ src/hw2/Task6.java
@@ -1,4 +1,13 @@
 package hw2;
 
 public class Task6 {
+    public static void main(String[] args) {
+        float z;
+        for (int i = 1; i <= 10; i++) {
+            z = i % 2;
+            if (z == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
