62c324c1c57e855d1d9d7601878ceba797777811
klandr <pozand47@gmail.com>
2022-05-30 17:12:01 +0200

    task 7 done

--- src/hw2/Task7.java
+++ src/hw2/Task7.java
@@ -3,19 +3,14 @@ package hw2;
 public class Task7 {
     public static void main(String[] args) {
         int a = 0;
-        int b = 0;
-        float z = 0 ;
-        for (int i = 1;i<=9;i++){
-        z = i % 2;
-
-        //b = a+i;
-        if (z == 1) { //yakshcho ne parne to
-            a = i;
-            System.out.println(a);
-            //System.out.println(b);
-            //System.out.println(a+b);
+        float z = 0;
+        for (int i = 1; i <= 11; i++) {
+            z = i % 2;
+            if (z == 1) { //yakshcho ne parne to dodayemo
+                a = i + a;
+                System.out.println(i);
             }
-                }
-        //System.out.println(a+b); summa chysel
+        }
+        System.out.println(a);  //suma chysel
     }
 }
