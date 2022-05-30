cb124bfd3503b660caf73f6ca5cc830891c58858
klandr <pozand47@gmail.com>
2022-05-30 17:30:14 +0200

    task 8 done

--- src/hw2/Task8.java
+++ src/hw2/Task8.java
@@ -1,4 +1,21 @@
 package hw2;
 
 public class Task8 {
+    public static void main(String[] args) {
+        int q = 0;
+        int w = 0;
+        float e = 0;
+        for (int i = 0;i<=9;i++){
+            e = i % 2;
+            if (e == 1) {
+                q = q + i;
+            }
+            else {
+                w = w + i;
+            }
+        }
+        System.out.println("summ of pairs = " + w);
+        System.out.println("summ of pairs = " + q);
+        System.out.println("summ of pairs or not pairs = " + (q+w));
+    }
 }
