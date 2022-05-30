d1cf5446e506bca0273d539c890e9cd72afe3476
klandr <pozand47@gmail.com>
2022-05-30 17:15:00 +0200

    task 7 done

--- src/hw2/Task7.java
+++ src/hw2/Task7.java
@@ -4,7 +4,7 @@ public class Task7 {
     public static void main(String[] args) {
         int a = 0;
         float z = 0;
-        for (int i = 1; i <= 11; i++) {
+        for (int i = 1; i <= 99; i++) {
             z = i % 2;
             if (z == 1) { //yakshcho ne parne to dodayemo
                 a = i + a;
