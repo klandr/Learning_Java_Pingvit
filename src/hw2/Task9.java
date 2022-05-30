11456f1f0825b43dc1ef7b24f7101244fdd8c61b
klandr <pozand47@gmail.com>
2022-05-30 17:47:14 +0200

    task 9 50/50

--- src/hw2/Task9.java
+++ src/hw2/Task9.java
@@ -3,10 +3,13 @@ package hw2;
 public class Task9 {
     public static void main(String[] args) {
         char a = '*';
-        //System.out.println(a);
-        for (int i =0;i<=4;i++)
+        System.out.println(a);
+        for (int i =0;i<=1;i++){
+            for (int u = 0; u<=1; u++)
+                System.out.print(a);
+        }
 
-            System.out.println(a);
+            //System.out.println(a);
 
     }
 }
