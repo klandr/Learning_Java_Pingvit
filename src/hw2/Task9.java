640ca17fe628b66c3cb1c70795e4a21c2a381cd7
klandr <pozand47@gmail.com>
2022-05-30 20:04:22 +0200

    task 9 krakaziabra

--- src/hw2/Task9.java
+++ src/hw2/Task9.java
@@ -3,13 +3,15 @@ package hw2;
 public class Task9 {
     public static void main(String[] args) {
         char a = '*';
-        System.out.println(a);
-        for (int i =0;i<=1;i++){
-            System.out.println(a);
-            for (int u = 0; u<=3; u++)
-                System.out.print(a);
-        }
+       System.out.println(a);
+        for (int i =1;i<=2;i++)
+            System.out.print(a);
 
+        for (int u = 1; u<=3; u++)
+            System.out.println(a);
+            /*    for (int y = 1; y<=2; y++)
+                    System.out.print(a);
+*/
             //System.out.println(a);
 
     }
