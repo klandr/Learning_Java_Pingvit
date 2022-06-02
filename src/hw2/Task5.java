6671fa3bba911502c6eaacb4d0d21936b504e0cf
Mary Stankevich <marrycitty@mail.ru>
2022-06-02 11:18:11 +0200

    Update Task5.java

--- src/hw2/Task5.java
+++ src/hw2/Task5.java
@@ -1,11 +1,12 @@
 package hw2;
 
+//+
 public class Task5 {
     public static void main(String[] args) {
-        double q = 2.54;
+        double q = 2.54; //имя переменной должно что-то значить 
         double w = 0;
         for (int i = 1; i <= 20; i++) {
-            w = q * i;
+            w = q * i; //hint: можно реализовать через сумму
             System.out.println(i + " inches = " + w + " cm");
         }
     }
