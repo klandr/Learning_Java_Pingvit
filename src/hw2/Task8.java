d5c580488459ef5ac2b7f3bb2b827c4ff705e64e
Mary Stankevich <marrycitty@mail.ru>
2022-06-02 11:25:54 +0200

    Update Task8.java

--- src/hw2/Task8.java
+++ src/hw2/Task8.java
@@ -1,10 +1,12 @@
 package hw2;
 
+//+-
 public class Task8 {
     public static void main(String[] args) {
-        int q = 0;
+        int q = 0; //название переменной должно что-то значить 
         int w = 0;
         float e = 0;
+        //Даны числа от 0 до 100, не до 9
         for (int i = 0;i<=9;i++){
             e = i % 2;
             if (e == 1) {
@@ -14,7 +16,7 @@ public class Task8 {
                 w = w + i;
             }
         }
-        System.out.println("summ of pairs = " + w);
+        System.out.println("summ of pairs = " + w); //четные - even, нечетные - odd
         System.out.println("summ of pairs = " + q);
         System.out.println("summ of pairs or not pairs = " + (q+w));
     }
