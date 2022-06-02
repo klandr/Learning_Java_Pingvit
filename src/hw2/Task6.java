15ee6e00f3c8d8eab0fc36764ad743420b7ea291
Mary Stankevich <marrycitty@mail.ru>
2022-06-02 11:19:42 +0200

    Update Task6.java

--- src/hw2/Task6.java
+++ src/hw2/Task6.java
@@ -1,10 +1,11 @@
 package hw2;
 
+//+-
 public class Task6 {
     public static void main(String[] args) {
-        float z;
+        float z; //остаток от деления - это целое число
         for (int i = 2; i <= 100; i++) {
-            z = i % 2;
+            z = i % 2; //можно написать без использования переменной 
             if (z == 0) {
                 System.out.println(i);
             }
