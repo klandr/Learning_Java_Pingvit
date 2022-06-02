f98277212f42d4f187c1916f45523c210fce6120
Mary Stankevich <marrycitty@mail.ru>
2022-06-02 10:45:56 +0200

    Update Task4.java

--- src/hw2/Task4.java
+++ src/hw2/Task4.java
@@ -1,12 +1,14 @@
 package hw2;
 
+//+
 public class Task4 {
     public static void main(String[] args) {
 
-        int a = 10; // chyslo A
+        int a = 10; // chyslo A <- комментарии стараемся писать на английском, но не латинкой
         int b = 8; // chyslo B
-        int d = 0; // dodatkowy parametr summa
-
+        int d = 0; // dodatkowy parametr summa <- может быть отображено в названии переменной result,  например
+        
+        //Hint: количество итераций может быть оптимизировано, если добавить одну проверку
         for (int i = 1; i <= b; i++) {
             d = d + a;
             //System.out.print(a + " * " + i + " = ");
