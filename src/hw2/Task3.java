16253369af19f83d235a2ae8995044f022166456
Mary Stankevich <marrycitty@mail.ru>
2022-06-02 10:41:24 +0200

    Update Task3.java

--- src/hw2/Task3.java
+++ src/hw2/Task3.java
@@ -1,13 +1,14 @@
 package hw2;
 
+/+- идея реализации хороша
 public class Task3 {
     public static void main(String[] args) {
-        //int i = 1;
+        //int i = 1; //не забываем чистить рабочие комментарии. Комментарии нужны для уточнения, что происходит в коде 
         int a = 0 ;
         for (int i = 1;i<=256;i=i+i) {
             //System.out.print(i + " lkjg ");
 
-            a=a+i;
+            a=a+i; //тут можно использовать а+=i;
            // System.out.println(a);
         }
         System.out.println("sum=" + a);
