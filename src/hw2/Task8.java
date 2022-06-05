2b470052a411b50c34a68ad77f4587d6b6c504d5
klandr <pozand47@gmail.com>
2022-06-05 23:39:27 +0200

    hw3 123_5

--- src/hw2/Task8.java
+++ src/hw2/Task8.java
@@ -3,21 +3,21 @@ package hw2;
 //+-
 public class Task8 {
     public static void main(String[] args) {
-        int q = 0; //название переменной должно что-то значить 
-        int w = 0;
+        int myOdd = 0; //название переменной должно что-то значить
+        int myEven = 0;
         float e = 0;
         //Даны числа от 0 до 100, не до 9
-        for (int i = 0;i<=9;i++){
+        for (int i = 0;i<=99;i++){
             e = i % 2;
             if (e == 1) {
-                q = q + i;
+                myOdd = myOdd + i;
             }
             else {
-                w = w + i;
+                myEven = myEven + i;
             }
         }
-        System.out.println("summ of pairs = " + w); //четные - even, нечетные - odd
-        System.out.println("summ of pairs = " + q);
-        System.out.println("summ of pairs or not pairs = " + (q+w));
+        System.out.println("summ of even numbers = " + myEven); //четные - even, нечетные - odd
+        System.out.println("summ of odd numbers = " + myOdd);
+        System.out.println("summ of even and odd numbers = " + (myOdd+myEven));
     }
 }
