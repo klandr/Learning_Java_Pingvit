d177092dc950959d089895c5c280c6c980d14d60
Mary Stankevich <marrycitty@mail.ru>
2022-06-08 21:03:25 +0200

    Update Task7.java

--- src/hw3/task_7/Task7.java
+++ src/hw3/task_7/Task7.java
@@ -7,11 +7,11 @@ public class Task7 {
 
         int[] nums = {9, 3, 1, 4, 6};
         int buff;
-        boolean repeat = true
+        boolean repeat = true  //не хватает точки с запятой, код не компилируется. переменная не используется. посмотри реализацию из лекции еще раз
 
         System.out.println(Arrays.toString(nums));
 
-        //for
+        //for <- удаляем лишние комментарии
         for (int i = 0; i < (nums.length-1); i++){
             if (nums[i] > nums[i+1]){
                 buff = nums[i];
