86fb0892ee2f2befccc8487ca9102818cbb5479a
Mary Stankevich <marrycitty@mail.ru>
2022-06-08 20:59:46 +0200

    Update Task6.java

--- src/hw3/task_6/Task6.java
+++ src/hw3/task_6/Task6.java
@@ -2,6 +2,7 @@ package hw3.task_6;
 
 import java.util.Arrays;
 
+//+ Код не отформатирован
 public class Task6 {
     public static void main(String[] args) {
 
@@ -17,10 +18,10 @@ public class Task6 {
         System.out.println("in given massive of "+array.length+" numbers");
         System.out.println("is " + summEven + " even");
 
-        int a = 0;
+        int a = 0; //используем более понятыне имена переменных, например, evenIndex
         int[] evenArray = new int[summEven];
 
-        for (int q = 0; q < array.length; q++){
+        for (int q = 0; q < array.length; q++){ //можно использовать ту же i
             if (array[q] % 2 == 0) {
                 evenArray[a] = array[q];
                 a = a +1;
