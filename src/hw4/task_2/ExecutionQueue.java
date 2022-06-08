67ed3c7cecfc6937269a65a3380f6faaf655112b
klandr <pozand47@gmail.com>
2022-06-09 00:38:38 +0200

    added package in hw2 & hw3

---
+++ src/hw4/task_2/ExecutionQueue.java
@@ -0,0 +1,23 @@
+package hw4.task_2;
+
+public class ExecutionQueue {
+    private static String msg;
+
+    static {
+        msg = "static block";
+    }
+
+    {
+        msg = "regular init lock";
+    }
+
+    public ExecutionQueue() {
+        msg = "constructor value";
+    }
+
+    public static void main(String[] args) {
+        ExecutionQueue executionQueue = new ExecutionQueue();
+        System.out.println(executionQueue.msg);
+    }
+
+}
