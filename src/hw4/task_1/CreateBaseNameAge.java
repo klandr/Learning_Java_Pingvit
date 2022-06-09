fca3df02b29123484ada94c034037a79dd071647
klandr <pozand47@gmail.com>
2022-06-09 17:48:37 +0200

    hw3

---
+++ src/hw4/task_1/CreateBaseNameAge.java
@@ -0,0 +1,9 @@
+package hw4.task_1;
+
+public class CreateBaseNameAge {
+    public static void main(String[] args) {
+        Unit Vasia = new Unit("Vasia",22);
+
+        System.out.println(Vasia.Show());
+    }
+}
