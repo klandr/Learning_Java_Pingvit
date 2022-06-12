009c671789c938655ea657273f2b5169f78dd726
Mary Stankevich <marrycitty@mail.ru>
2022-06-12 10:24:21 +0200

    Update CreateBaseNameAge.java

--- src/hw4/task_1/CreateBaseNameAge.java
+++ src/hw4/task_1/CreateBaseNameAge.java
@@ -2,6 +2,7 @@ package hw4.task_1;
 
 public class CreateBaseNameAge {
     public static void main(String[] args) {
+        //Имя переменной должно быть с маленькой буквы 
         Unit Vasia = new Unit("Vasia",22);
 
         System.out.println(Vasia.Show());
