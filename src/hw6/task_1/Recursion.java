fa406f30b84463bfc1f4de7767abb343d8248245
klandr <pozand47@gmail.com>
2022-06-15 16:17:15 +0200

    hw6 t1 done

--- src/hw6/task_1/Recursion.java
+++ src/hw6/task_1/Recursion.java
@@ -1,28 +1,24 @@
 package hw6.task_1;
 
+import java.io.OptionalDataException;
 import java.util.Arrays;
 
 //Task 1
-//Написать рекурсивную функцию, которая бы перебирала все элементы массива и выводила их на экран
+//Написать рекурсивную функцию, которая бы перебирала все элементы массива
+// и выводила их на экран
 public class Recursion {
-    public static void main(String[] args) {
+    public static void main(String[] args) {      //creating massive with numbers
         int[] numbers = {7, 3, 2, 4, 5, 3, 2, 9};
-        System.out.println(numbers.length);
-        int proba_lenght = numbers.length;
-        System.out.println(proba_lenght);
-        System.out.println("numbers[0] = " + numbers[0]);
         int x = 0;
-        System.out.println("x default = " + x);
-        counting(x);
+        counting(x, numbers);                     // initialling x, numbers
     }
 
-    private static int counting(int x) {
-        System.out.println("helo from counting x = " + x);
-        //System.out.println(numbers[0]);
-        if (x == 3) {
+    public static int counting(int x, int[] numbers) { // initialling x, numbers
+        //System.out.println("hello from counting x = " + x);
+        System.out.print(numbers[x] + " ");
+        if (x == (numbers.length - 1)) {
             return x;
         }
-    return counting(++x);
+        return counting(++x, numbers); // initialling x, numbers
     }
-
 }
