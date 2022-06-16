0c38cf09ebc388fcaea28bf12040f164283dca6a
klandr <pozand47@gmail.com>
2022-06-16 06:04:53 +0200

    hw6 t2 divination

--- src/hw6/task_2/Engine.java
+++ src/hw6/task_2/Engine.java
@@ -3,25 +3,20 @@ package hw6.task_2;
 public class Engine {
     private String stopEngine;
     private String startEngine;
-
-//    public Engine(String stopEngine, String startEngine) {
-//    this.stopEngine = stopEngine;
-//    this.startEngine = startEngine;
-//    }
-
-    public String getStopEngine(){                      //|get  |
+    public String getStopEngine() {
+        System.out.println("print get Stop engine");    //|get  |
         return stopEngine;                              //|     |
-    }                                                   //|     |
-                                                        //|     |     Stop engine
-    public void setStopEngine(String stopEngine){       //|set  |
+    }                                                   //|     |     Stop engine
+     public void setStopEngine(String stopEngine) {
+        System.out.println("print set Stop engine");    //|set  |
         this.stopEngine = stopEngine;                   //|     |
     }                                                   //|     |
-
-    public String getStartEngine(){                    //|get  |
+    public String getStartEngine() {
+        System.out.println("print get Start engine");   //|get  |
         return startEngine;                             //|     |
-    }                                                   //|     |
-                                                        //|     |   Start engine
-    public void setStartEngine (String startEngine){    //|set  |
+    }                                                   //|     |   Start engine
+    public void setStartEngine(String startEngine) {
+        System.out.println("print set Start engine");   //|set  |
         this.startEngine = startEngine;                 //|     |
     }                                                   //|     |
 }
