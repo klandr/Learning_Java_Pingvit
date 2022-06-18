6711bfe9ffd16f5c9c2500d5a5a8c4609e5a122c
klandr <pozand47@gmail.com>
2022-06-18 16:33:36 +0200

    hw7 t123

---
+++ src/hw7/task_1.java
@@ -0,0 +1,10 @@
+package hw7;
+
+public class task_1 {
+    public static void main(String[] args) {
+        String myText = "engineer";
+
+        System.out.println(myText.substring(0, 6));
+        System.out.println(myText.charAt(myText.length()-1)+myText.substring(3,5)+myText.charAt(2));
+     }
+}
---
+++ src/hw7/task_2.java
@@ -0,0 +1,14 @@
+package hw7;
+
+public class task_2 {
+    public static void main(String[] args) {
+       String someWord = "alucard";
+        System.out.println(someWord.length());
+
+        for (int i=someWord.length()-1;i>=0;i--) {
+            System.out.print(someWord.charAt(i));
+        }
+
+
+    }
+}
