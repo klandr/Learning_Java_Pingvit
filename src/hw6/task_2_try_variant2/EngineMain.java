754b9c199e8d448bd0a44c75836e75096604556e
Mary Stankevich <marrycitty@mail.ru>
2022-06-18 21:28:59 +0200

    Update EngineMain.java

--- src/hw6/task_2_try_variant2/EngineMain.java
+++ src/hw6/task_2_try_variant2/EngineMain.java
@@ -1,5 +1,6 @@
 package hw6.task_2_try_variant2;
 
+//+- как тестовый класс может быть, но интересует как двигатель будет вести себя в машине
 public class EngineMain {
     public static void main(String[] args) {
         EngineSteer engineSteer = new EngineSteer();
