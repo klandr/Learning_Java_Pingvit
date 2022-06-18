759e3c4fa766067d8f01324186908a9c9f109d88
Mary Stankevich <marrycitty@mail.ru>
2022-06-18 21:26:25 +0200

    Update Engine.java

--- src/hw6/task_2_try_variant2/Engine.java
+++ src/hw6/task_2_try_variant2/Engine.java
@@ -1,5 +1,6 @@
 package hw6.task_2_try_variant2;
 
+//+- все же это должен быть класс, чтобы его сразу использовать. Но вариант может быть, если двигатели будут разных типов
 public interface Engine {
     void startEngine();
     void stopEngine();
