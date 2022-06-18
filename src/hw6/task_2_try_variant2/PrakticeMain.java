4f8a7453121a9bca32968ca20ccea8320c316aaf
Mary Stankevich <marrycitty@mail.ru>
2022-06-18 21:31:30 +0200

    Update PrakticeMain.java

--- src/hw6/task_2_try_variant2/PrakticeMain.java
+++ src/hw6/task_2_try_variant2/PrakticeMain.java
@@ -1,5 +1,6 @@
 package hw6.task_2_try_variant2;
 
+//Я так понимаю, что этот класс тут случайно оказался 
 public class PrakticeMain {
     public static void main(String[] args) {
         String name = "Egorka";
