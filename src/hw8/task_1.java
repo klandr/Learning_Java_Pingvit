8188c6aef44a8085dd2e46d5bc6cb13fd579582c
klandr <pozand47@gmail.com>
2022-06-22 23:07:27 +0200

    hw8 t1

--- src/hw8/task_1.java
+++ src/hw8/task_1.java
@@ -3,20 +3,18 @@ package hw8;
 public class task_1 {
     static int myDivide(int a, int b) {
 
-        if ( b !=0) {
+        if (b != 0) {
             int x;
             x = a / b;
             return x;
-        }
-        else {
+        } else {
             System.out.println(" ay yai yai your mum don't afford you divide on 0  ");
             return b; // i don't understand for what this return
         }
     }
 
-
     public static void main(String[] args) {
-        System.out.println(myDivide(6,2));
-        System.out.println(myDivide(7,0));
+        System.out.println(myDivide(6, 2));
+        System.out.println(myDivide(7, 0));
     }
 }
