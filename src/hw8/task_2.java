dbe9de52cd334acd02170c14653c680d35fc3a23
klandr <pozand47@gmail.com>
2022-06-23 00:18:12 +0200

    h8 t2

--- src/hw8/task_2.java
+++ src/hw8/task_2.java
@@ -1,4 +1,9 @@
 package hw8;
+//Написать метод, который бы парсил строку в число, обработать исключение,
+// которое может быть при неверном формате числа
 
 public class task_2 {
+    public static void main(String[] args) {
+      //  String
+    }
 }
