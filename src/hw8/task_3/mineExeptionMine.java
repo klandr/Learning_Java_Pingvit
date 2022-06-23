a1130aed93b47c6ee337035f4f89c85da943918c
klandr <pozand47@gmail.com>
2022-06-23 23:33:19 +0200

    hw8 t3 done

--- src/hw8/task_3/mineExeptionMine.java
+++ src/hw8/task_3/mineExeptionMine.java
@@ -1,36 +1,36 @@
 package hw8.task_3;
 
-import com.sun.jdi.Value;
-
 import java.util.Arrays;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
+//Попросите пользователя ввести число размера будущего массива, но не больше размера Y (задайте сами).
+//Если пользователь ввел число больше Y или отрицательное, то выбросьте собственное исключение и
+//попросите пользователя ввести число заново.
 public class mineExeptionMine {
     public static void main(String[] args) throws mineExeption {
         Scanner scanNumber = new Scanner(System.in);
         String numberSizeArray = null;// i need another type from int,  which can apply the number and the letter
+        int valueOfMassive = 5;
 
         try {
-            System.out.println("Enter number between 0 and 5, please");
+            System.out.println("Enter number between 0 and " +valueOfMassive+", please");
             numberSizeArray = scanNumber.next();
             int a = Integer.valueOf(numberSizeArray);
 
-            if ((a > 5) || (a < 0)) {
-                System.out.println("your sign = " + numberSizeArray + " Re Enter number between 0 and 5, please");
+            if ((a > valueOfMassive) || (a < 0)) {
+                System.out.println("your sign = " + numberSizeArray + " Re Enter number between 0 and " +valueOfMassive+", please");
                 return;
             }
 
             int[] myArray = new int[a];
             System.out.println(Arrays.toString(myArray));
             System.out.println("thanks");
-            System.out.println(numberSizeArray);
 
         } catch (NumberFormatException e) {
             try {
                 throw new mineExeption("Enter number", numberSizeArray);//i just print 0
             } catch (mineExeption ee) {
-                System.out.println("your sign = " + ee.getCheckNumber() + " Enter number between 0 and 5, please");
+                System.out.println("your sign = " + ee.getCheckNumber() + " Enter number between 0 and " +valueOfMassive+", please");
             }
         }
     }
--- src/hw8/task_3/task_3.java
+++ src/hw8/task_3/task_3.java
@@ -7,31 +7,25 @@ import java.util.Scanner;
 //Попросите пользователя ввести число размера будущего массива, но не больше размера Y (задайте сами).
 //Если пользователь ввел число больше Y или отрицательное, то выбросьте собственное исключение и
 //попросите пользователя ввести число заново.
+
 public class task_3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //without own Exception
         Scanner scanNumber = new Scanner(System.in);
 
-        try{
+        try {
             System.out.println("Enter number between 0 and 5, please");
             int numberSizeArray = scanNumber.nextInt();
             int[] myArray = new int[numberSizeArray];
             System.out.println(Arrays.toString(myArray));
-            // System.out.println(scanNumber);
             System.out.println("thanks");
             System.out.println(numberSizeArray);
-        }
-        catch (InputMismatchException e) {
+        } catch (InputMismatchException e) {
 
             System.out.println("your sign isn't a number");
-            //System.out.println(e);
-            //System.out.println();
         }
-
 
 
     }
-
-
 
 
 }
