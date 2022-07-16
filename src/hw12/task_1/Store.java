6b18bc0f75b18c2768ab88b717b1180d3ac6ef86
Mary Stankevich <marrycitty@mail.ru>
2022-07-16 11:50:38 +0200

    Update Store.java

--- src/hw12/task_1/Store.java
+++ src/hw12/task_1/Store.java
@@ -5,12 +5,15 @@ import java.util.Date;
 
 public class Store {
     public static void main(String[] args) {
-        Date date = new Date();
+        //Date - устарешвий тип,используем LocalDateTime
+        Date date = new Date(); //LocalDateTime.now()
+        //В коллекции должны быть объекты, которые хронят значение (имя) и время LocalDateTime (created)
         String chicken = new String("Kura");
         String milk = new String("Mleko");
         String eggs = new String("Jajko");
-
+        
         ArrayList<String> storeList = new ArrayList<>();
+        //такое храннение усложнит работу с объектом, т.е. для извлечения имени потребуются дополнитеьные действия - используй объект, а не просто строку
         storeList.add((chicken)+" "+date);
         storeList.add((milk)+" "+date);
         storeList.add((eggs)+" "+date);
